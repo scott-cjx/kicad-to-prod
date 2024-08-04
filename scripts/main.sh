c77405b99fa711cbd8c37a0975a166bb299b9339
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-08-05 02:11:19 +0800

    updated workflow

--- scripts/main.sh
+++ scripts/main.sh
@@ -2,6 +2,8 @@
 
 # Create output directories if they don't exist
 
+mkdir -p /ws/docs/checks
+mkdir -p /ws/docs/pcb
 mkdir -p /ws/docs/sch
 cd /ws/docs/sch
 
@@ -9,10 +11,8 @@ cd /ws/docs/sch
 kicad-cli sch export pdf /ws/HDK/*kicad_sch 
 kicad-cli sch export svg /ws/HDK/*kicad_sch
 
-mkdir -p /ws/docs/erc/checks
+mkdir -p /ws/docs/checks/erc
 cd /ws/docs/checks/erc
 
 # erc of schematics
 kicad-cli sch erc /ws/HDK/*kicad_sch
-
-mkdir -p /ws/docs/pcb
