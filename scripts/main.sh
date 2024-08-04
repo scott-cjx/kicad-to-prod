ce317aec11f0244c28ec86b3d1b3ec9778e4e69d
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-08-05 02:46:07 +0800

    updated workflow

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 # export schematics
-kicad-cli sch export pdf /ws/HDK/*kicad_sch --output sch
-kicad-cli sch export svg /ws/HDK/*kicad_sch --output sch
+kicad-cli sch export pdf /ws/HDK/*kicad_sch --output sch/sch.pdf
+kicad-cli sch export svg /ws/HDK/*kicad_sch --output sch/
 
 # erc of schematics
-kicad-cli sch erc /ws/HDK/*kicad_sch --output checks
+kicad-cli sch erc /ws/HDK/*kicad_sch --output checks/erc.rpt
