3b4b0fb59796a22f7b97c3784062949cc7961d89
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-08-05 04:15:27 +0800

    updated docs workflow

--- scripts/collate_schsvg.py
+++ scripts/collate_schsvg.py
@@ -16,6 +16,7 @@ with open(md_file, 'w') as f:
     for svg_file in svg_files:
         file_name = os.path.splitext(svg_file)[0]
         image_path = os.path.join(svg_dir, svg_file)
-        f.write(f'![{file_name}]({image_path}) \n\n')
+        refactored_image_path = os.path.join(svg_file)
+        f.write(f'![{file_name}]({refactored_image_path}) \n\n')
 
 print(f"Markdown document created at {md_file}")
