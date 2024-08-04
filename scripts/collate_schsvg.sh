91cfd1ec8c0c9c514057a84e6840f41584767d20
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-08-05 03:43:07 +0800

    trying out collate sch

--- scripts/collate_schsvg.sh
+++ scripts/collate_schsvg.sh
@@ -8,13 +8,13 @@ md_file="/ws/docs/sch/sch.md"
 echo "# Collected Schematics Images \n\n" > "$md_file"
 
 # Loop through all SVG files in the directory
-for svg_file in "$svg_dir"/*.svg; do
+for svg_file in $(ls -v "$svg_dir"/*.svg | tac); do
   # Extract filename without extension
   file_name=$(basename "$svg_file" .svg)
   refactored_file_name=$(basename "$svg_file" .svg)
 
   # Add image to Markdown file
-  echo "![${file_name}](${refactored_file_name}.svg) \n\n" >> "$md_file"
+  echo "![${file_name}]("${refactored_file_name}.svg") \n\n" >> "$md_file"
 done
 
 echo "Markdown document created at $md_file"
