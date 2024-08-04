7b330058f5d350b955ff9345d78ef52a366abfe2
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-08-05 02:54:10 +0800

    updated workflow

--- dockerfile
+++ dockerfile
@@ -3,18 +3,20 @@ FROM kicad/kicad:nightly-full
 LABEL author="Scott CJX"
 LABEL version="1.0.0"
 
+# make container root user
+USER 0
+
 # Set environment variables if necessary
 ENV DISPLAY=:0
 
 WORKDIR /ws
 
+RUN mkdir -p /ws/docs/
+
 COPY HDK HDK
 COPY docs docs
 COPY scripts scripts
 
-WORKDIR /ws/docs/checks
-WORKDIR /ws/docs/sch
-WORKDIR /ws/docs/pcb
 WORKDIR /ws/docs
 
 CMD ["sh", "/ws/scripts/main.sh"]
--- scripts/main.sh
+++ scripts/main.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
-# export schematics
-kicad-cli sch export pdf /ws/HDK/*kicad_sch --output sch/sch.pdf
-kicad-cli sch export svg /ws/HDK/*kicad_sch --output sch/
+mkdir -p /ws/docs/sch/
+mkdir -p /ws/docs/pcb/
+mkdir -p /ws/docs/checks/
 
+# export schematics
+touch /ws/docs/sch/sch.pdf
+kicad-cli sch export pdf /ws/HDK/*kicad_sch --output /ws/docs/sch/sch.pdf
+kicad-cli sch export svg /ws/HDK/*kicad_sch --output /ws/docs/sch/
+
+touch /ws/docs/checks/erc.rpt
 # erc of schematics
-kicad-cli sch erc /ws/HDK/*kicad_sch --output checks/erc.rpt
+kicad-cli sch erc /ws/HDK/*kicad_sch --output /ws/docs/checks/erc.rpt
