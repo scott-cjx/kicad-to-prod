9f5081722b68c38cc06733e1a9712d3d781033de
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-08-05 03:07:58 +0800

    added features

--- scripts/main.sh
+++ scripts/main.sh
@@ -9,15 +9,17 @@ touch /ws/docs/sch/sch.pdf
 kicad-cli sch export pdf /ws/HDK/*kicad_sch --output /ws/docs/sch/sch.pdf
 kicad-cli sch export svg /ws/HDK/*kicad_sch --output /ws/docs/sch/
 
-touch /ws/docs/checks/erc.rpt
 # erc of schematics
 kicad-cli sch erc /ws/HDK/*kicad_sch --output /ws/docs/checks/erc.json --format json
 
-# export pcb
-kicad-cli pcb export pdf /ws/HDK/*kicad_pcb --output /ws/docs/pcb/pcb.pdf
-kicad-cli pcb export svg /ws/HDK/*kicad_pcb --output /ws/docs/pcb/
+# # export pcb
+# kicad-cli pcb export pdf /ws/HDK/*kicad_pcb --output /ws/docs/pcb/pcb.pdf
+# kicad-cli pcb export svg /ws/HDK/*kicad_pcb --output /ws/docs/pcb/
+
+kicad-cli pcb drc /ws/HDK/*kicad_pcb --output /ws/docs/checks/drc.json --format json
 
 mkdir -p /ws/docs/pcb/step
-kicad-cli pcb drc /ws/HDK/*kicad_pcb --output /ws/docs/checks/drc.json --format json
+kicad-cli pcb export step /ws/HDK/*kicad_pcb --output /ws/docs/pcb/
+
 
 # drc of pcb
