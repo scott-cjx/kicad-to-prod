9422bd3c3fe2b74f82b58b43f9052ae320affb75
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-08-05 03:51:50 +0800

    used python for collation instead

--- scripts/collate_schsvg.py
+++ scripts/collate_schsvg.py
@@ -2,7 +2,7 @@ import os
 
 # Define the directories
 svg_dir = '/ws/docs/sch'
-md_file = os.path.join(svg_dir, 'collected_images.md')
+md_file = os.path.join(svg_dir, 'README.md')
 
 # Get a list of SVG files and sort them in reverse order
 svg_files = [f for f in os.listdir(svg_dir) if f.endswith('.svg')]
@@ -16,6 +16,6 @@ with open(md_file, 'w') as f:
     for svg_file in svg_files:
         file_name = os.path.splitext(svg_file)[0]
         image_path = os.path.join(svg_dir, svg_file)
-        f.write(f"![{file_name}]({image_path})\n\n")
+        f.write(f"![{file_name}]('{image_path}.svg')\n\n")
 
 print(f"Markdown document created at {md_file}")
